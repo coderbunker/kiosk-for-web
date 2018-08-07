40b09ebacf1035fc25069ea1608c820544d99b8b
dennismatter <dennismatter.bbb@gmail.com>
2018-08-07 09:25:00 +0200

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Demo from './Demo';
+import '../css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <p> </p>
+          <h1 className="App-title">Video Demo</h1>
+        </header>
+        <p className="App-intro">
+        <Demo/>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
+//<Demo/>
---
+++ src/components/Demo.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactPlayer from 'react-player';
+import { findDOMNode } from 'react-dom'
+import screenfull from 'screenfull'
+
+import { PlayButton, PauseButton } from 'react-player-controls'
+
+class Demo extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            width: '100%',
+            videoUrl: '../media/mov_bbb.mp4',
+            playing: false,
+        };
+
+
+    }
+    onClickFullscreenStart = () => {
+        screenfull.request(findDOMNode(this.refs.player)), this.setState({playing: true})
+      }
+
+
+
+    render() {
+        return (
+            <div className={"demo"}>
+
+                    <ReactPlayer ref='player' className="full-screenable-node" url={require('../media/mov_bbb.mp4')} playing={this.state.playing} loop={true} width={this.state.width} />
+                    <button className={"btn-player"} onClick={this.onClickFullscreenStart}> play </button>
+                   
+            </div>
+                );
+            }
+        }
+        
+        export default Demo;
